13db37a64b17071219abb7703c5490eb0f903073
vinayakgarole <vinayakgarole10@gamil.com>
2023-04-07 10:46:32 +0530

    Modified: Creating a simple servlet for login page

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,6 +1,4 @@
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,34 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(
-        description = "Login Servlet Testing",
-        urlPatterns = {"/LoginServlet"},
-        initParams = {
-                @WebInitParam(name = "user", value = "Vinayak"),
-                @WebInitParam(name = "password", value = "123")
-        }
-)
-
-public class LoginServlet extends HttpServlet {
+@WebServlet( "/FirstServlet" )
+class FirstServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        //get request parameters for userid and Password
-        String user = request.getParameter("user");
-        String pwd = request.getParameter("pwd");
-        //get servlet config init params
-        String userId = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        if (userId.equals(user) && password.equals(pwd)) {
-            request.setAttribute("user", user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
-        } else {
-            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/Login.html");
-            PrintWriter out = response.getWriter();
-            out.println("<font color = red> Either user name or password is wrong.</font>");
-            requestDispatcher.include(request, response);
-        }
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h3>Hello World! Welcome to Servlet Programming!");
+        out.println("<p>Welcome for my First server");
+        out.close();
     }
 }
